21a9b36312477c602e87786489084efcd996006c
federico-silva-lenio <federico.silva@leniolabs.com>
2023-06-02 21:30:45 -0300

    Hello World x2

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def print_hi(name):
     print(name)
     print('Hi')
     print('World')
+    print('Mars')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
